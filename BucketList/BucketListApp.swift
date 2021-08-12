0e966c7379ec39ede481cff7a95d593db7cf3e41
Olivier Van hamme <nicefiction@gmail.com>
2021-08-12 09:18:35 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Olivier Van hamme on 12/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
